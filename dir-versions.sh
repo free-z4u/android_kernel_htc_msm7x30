48805fe74b60193d421109159ba00df1840b2c63
Denis Pauk <pauk.denis@gmail.com>
2016-10-27 23:29:31 +0300

    show names in msm_clocks_7x30

--- dir-versions.sh
+++ dir-versions.sh
@@ -1,5 +1,5 @@
 ####v3.17
-git checkout HEAD -- crypto/sha512_generic.c arch/arm/crypto/aes-armv4.S  arch/arm/crypto/aes_glue.c  arch/arm/crypto/sha1-armv4-large.S arch/arm/crypto/sha1_glue.c
+git checkout HEAD -- arch/arm/crypto/aes-armv4.S
 
 ####v3.18
 git checkout HEAD -- lib/lzo/lzo1x_decompress_safe.c
